f6c7c04b555acdcc7e23f2dc68e0544c88a16171
e1016 <e10169610@gmail.com>
2019-07-03 01:33:14 -0600

    iOS improvements

---
+++ README.md
@@ -0,0 +1,3 @@
+## Native Touchable for React Native
+
+> this Touchable isn't native, it's a fork of [react-native-material-ripple](https://www.npmjs.com/package/react-native-material-ripple) by [@n4kz](https://github.com/n4kz)
--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const Touchable = props => (Platform.OS === 'android' || props.waves) ? (
   </RippleTouchable>
 ) : (
   // iOS implements
-  <TouchableOpacity activeOpacity={ 0.7 } { ...props }>
+  <TouchableOpacity activeOpacity={ 0.8 } { ...props }>
     { props.children }
   </TouchableOpacity>
 )
